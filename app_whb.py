68946d6f7823a64299bf73b1c6f45c6119f88fe5
DESKTOP-7LSDEJ0\chohunki <chk4814@juvis.co.kr>
2022-10-25 16:29:57 +0900

    work jwt ing

--- app_whb.py
+++ app_whb.py
@@ -8,15 +8,35 @@ from functools import wraps
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "kcKaoo4Md-riOdVU"
 
-client = MongoClient('mongodb+srv://test:<password>@cluster0.cbhgxgw.mongodb.net/?retryWrites=true&w=majority')  ##
+client = MongoClient('localhost', 27017)  ##
 db = client.week0  # 'week0'라는 이름의 db를 만들거나 사용합니다.
 
+def token_required(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = request.cookies.get('token')
+        print(token)
+        if not token:
+            print("step1")
+            print(jsonify({'Alert!': 'token is missing!'}), 401)
+            return render_template('loginform.html')
+        try:
+            print("step2")
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except:
+            print("step3")
+            return render_template('loginform.html')
+        return func(*args, **kwargs)
+    return decorated
+
 @app.route('/')
+@token_required
 def home():
     if not session.get('logged_in'):
         return render_template('loginform.html')
     else: 
-        return 'Logged in currently!'
+        jwt_id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        return render_template('main.html',id = jwt_id)
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -30,29 +50,21 @@ def login():
         session['logged_in'] = True
 
         token = jwt.encode({
-            'user': request.form['username'],
+            'id': request.form['id'],
             # don't foget to wrap it in str function, otherwise it won't work [ i struggled with this one! ]
             'expiration': str(datetime.utcnow() + timedelta(seconds=60))
         },
             app.config['SECRET_KEY'])
-        return jsonify({'token': token.decode('utf-8')})
+        print('asdasd')
+        #print(jsonify({'token': token}))
+        resp = make_response("/home") 
+        resp.set_cookie('token',token)
+        return resp
+        #return jsonify({'result': 'success', 'token': token})
     else:
         print("실패")
-        return make_response('Unable to verify', 403, {'WWW-Authenticate': 'Basic realm: "Authentication Failed "'})
-
-def token_required(func):
-    @wraps(func)
-    def decorated(*args, **kwargs):
-        tocken=request.args.get('token')
-        if not token:
-            return jsonify({'Alert!': 'token is missing!'}), 401
-        try:
-            data = jwt.decode(token, app.config['SECRET_KEY'])
-        except:
-            return jsonify({'Message': 'Invalid token'}), 403
-        return func(*args, **kwargs)
-    return decorated
-
+        print(make_response('Unable to verify', 403, {'WWW-Authenticate': 'Basic realm: "Authentication Failed "'}))
+        return render_template('loginform.html')
 
 
 if __name__ == '__main__':
