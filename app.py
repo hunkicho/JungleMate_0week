610e1d09c704de922d89f10b6076683d8858841c
DESKTOP-7LSDEJ0\chohunki <chk4814@juvis.co.kr>
2022-10-25 14:09:18 +0900

    session complete

--- app.py
+++ app.py
@@ -12,22 +12,6 @@ db = client.week0  # 'week0'라는 이름의 db를 만들거나 사용합니다.
 def loginform():
     return render_template('loginform.html')
 
-@app.route('/login', methods=['POST'])
-def login():
-    receive_id = request.form['id']  # 클라이언트로부터 id 받는 부분
-    receive_password = request.form['password']  # 클라이언트로부터 password 받는 부분
-
-    result = db.users.find_one({'id' : receive_id, 'password' : receive_password},{'_id' : False, 'id' : True}) #id와 pw가 일치하는 값 찾기
-
-    if result == None: #일치하는 값이 없을 경우
-        print("실패")
-    else:
-        print("성공")  #일치하는 값이 있으면
-        session['id'] = result['id'] #세션에 아이디로 정보 저장
-        main() #메인페이지로 이동
-
-    return render_template('test.html')
-
 @app.route('/main')
 def main():
     #session.pop('id',None)
@@ -36,14 +20,30 @@ def main():
 
     return render_template('main.html',id = sess_id)
 
+@app.route('/login',  methods=['POST','GET'])
+def login():
+    receive_id = request.form['id']  # 클라이언트로부터 id 받는 부분
+    receive_password = request.form['password']  # 클라이언트로부터 password 받는 부분
+
+    print(receive_id)
+
+    result = db.users.find_one({'id' : receive_id, 'password' : receive_password},{'_id' : False, 'id' : True}) #id와 pw가 일치하는 값 찾기
+
+    if result == None: #일치하는 값이 없을 경우
+        print("실패")
+        return render_template('loginform.html')
+    else:
+        print("성공")  #일치하는 값이 있으면
+        session['id'] = result['id'] #세션에 아이디로 정보 저장
+        return main() #메인페이지로 이동
+        
 
 def sess_check():
     sess_id = session['id'] #세션값 가져오기
     if sess_id == "": #없으면 로그인 페이지로
         return render_template('loginform.html')
     else:
-        return True
-
+        return "1"
 
 
 if __name__ == '__main__':
